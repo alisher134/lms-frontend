1d5d3f502d7a933dd4c92d2a628bf7fc838e745a
alisher134 <alisherrakhmanov134@gmail.com>
2026-06-30 21:41:17 +0500

    chore(setup): add prettier, eslint, husky configs and tailwindcss, hero ui

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,12 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import App from './app/app.tsx';
+import './app/index.css';
 
-createRoot(document.getElementById('root')!).render(
+const rootElement = document.getElementById('root');
+
+createRoot(rootElement!).render(
   <StrictMode>
     <App />
   </StrictMode>,
-)
+);
